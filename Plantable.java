66f436616c286d25e1c326f041bddcdb3fc67ca9
Антон <anton200827@gmail.com>
2025-12-23 20:51:34 +0300

    Delete Plantable.java

--- Plantable.java
+++
@@ -1,7 +0,0 @@
-
-public interface Plantable { //Интерфейс "сажать растения" для Карлсона
-
-    void putSeedInPot(Pot pot); //Чтобы сажать растения, нужно положить семечко в горшок
-
-    void coverWithSoil(Pot pot); //И засыпать землёй
-}
